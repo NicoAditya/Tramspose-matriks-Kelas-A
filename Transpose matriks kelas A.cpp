201214b7609375e147bdeb00f69ce16f9beb4c92
NicoAditya <62372488+NicoAditya@users.noreply.github.com>
2020-03-21 20:39:52 +0700

    Update Transpose matriks kelas A.cpp

--- Transpose matriks kelas A.cpp
+++ Transpose matriks kelas A.cpp
@@ -1,2 +1,8 @@
-#include <iostream>
-
+#include<iostream>
+using namespace std;
+int main(){
+   int matrix[10][10], transMatrix[10][10], row, col;
+   cout<<"Enter the number of rows: ";
+   cin>>row;
+   cout<<"Enter the number of columns: ";
+   cin>>col;
