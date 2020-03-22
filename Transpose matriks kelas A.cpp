f8fd4995203c64dbf52ac87235910795dc89be97
Deviramadhia82 <62375133+Deviramadhia82@users.noreply.github.com>
2020-03-22 09:25:59 +0700

    Update Transpose matriks kelas A.cpp

--- Transpose matriks kelas A.cpp
+++ Transpose matriks kelas A.cpp
@@ -13,5 +13,9 @@ int main(){
        cin >> matrix [i][j];
      }
    }
-   
+   for(int i = 0; i < row; i++) {
+    for(int j = 0; j < col; j++) {
+      transMatrix [j][i] = matrix [i][j];
+    }
+  }
    
